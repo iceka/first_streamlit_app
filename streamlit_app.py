3c7d5785f594a9b464ef1ea926bdfc441f37ee6e
Imam Candra Kusuma <53084585+iceka@users.noreply.github.com>
2023-05-01 23:48:57 +0700

    Update streamlit_app.py
    add new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,7 @@ fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruit_to_show = my_fruit_list.loc[fruit_selected]
 # Display the table on the page.
 streamlit.dataframe(fruit_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
