b1b7f18fcd9353ea8007b47f5914deda4f9e75ba
Imam Candra Kusuma <53084585+iceka@users.noreply.github.com>
2023-05-01 21:41:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,4 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruit_to_show = my_fruit_list.loc[fruit_selected]
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruit_to_show)
